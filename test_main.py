5152a50a7f7232b1758e7ac589ad7abbb20eef6e
zudduz <zudduz@gmail.com>
2025-12-03 05:59:33 +0000

    feat: Add test stage to Cloud Build pipeline

--- test_main.py
+++ test_main.py
@@ -7,11 +7,11 @@ class TestParseAIResponse(unittest.TestCase):
     def test_parse_valid_response(self):
         response_string = 'Through whimsical chaos, a truth shall unwind,\nA journey of self, a curious kind.\nIn laughter and lightness, you\'ll find the key,\nTo unlock the person you\'re destined to be.Your vision for the future has been generated.{"vision_text": "Through whimsical chaos, a truth shall unwind,\\nA journey of self, a curious kind.\\nIn laughter and lightness, you\'ll find the key,\\nTo unlock the person you\'re destined to be.", "image_url": "https://storage.googleapis.com/sandbox-456821-oracle-visions/visions/ca27a9e3-97f5-49e5-bc84-9137cbc0d90c.png"}'
         expected_output = {
-            "vision_text": "Through whimsical chaos, a truth shall unwind,\n" \
+            "text": "Through whimsical chaos, a truth shall unwind,\n" \
                          "A journey of self, a curious kind.\n" \
                          "In laughter and lightness, you\'ll find the key,\n" \
                          "To unlock the person you\'re destined to be.",
-            "image_url": "https://storage.googleapis.com/sandbox-456821-oracle-visions/visions/ca27a9e3-97f5-49e5-bc84-9137cbc0d90c.png"
+            "image": "https://storage.googleapis.com/sandbox-456821-oracle-visions/visions/ca27a9e3-97f5-49e5-bc84-9137cbc0d90c.png"
         }
         self.assertEqual(parse_ai_response(response_string), expected_output)
 
